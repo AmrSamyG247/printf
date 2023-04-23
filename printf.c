2aa512c53e8c2f756b39b9fcfff7d5b5614188da
AmrGreens247 <amrsamy111@gmail.com>
2023-04-23 07:36:17 -0700

    Printf task update functions and tested gcc

--- printf.c
+++ printf.c
@@ -1,287 +1,43 @@
 #include "main.h"
 /**
- * _printf - function my printf
- * @format: string whit format to print
- *
- * Return: number of chars that print
+ * _printf - prints any string with certain flags for modification
+ * @format: the string of characters to write to buffer
+ * Return: an integer that counts how many writes to the buffer were made
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int length = 0;
+	int i = 0, var = 0;
+	va_list v_ls;
+	buffer *buf;
 
+	buf = buf_new();
+	if (buf == NULL)
+		return (-1);
 	if (format == NULL)
 		return (-1);
-
-	va_start(args, format);
-
-	length = _print_format(format, args);
-	va_end(args);
-	return (length);
-}
-
-/**
- * _write - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _write(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
-  * _print_int_binary - Prints a int converted to binary
-  * @args: A list of variadic arguments
-  *
-  * Return: The number of printed digits
-  */
-int _print_int_binary(va_list args)
-{
-	unsigned int x = 0;
-	int b = 0, new = 0;
-
-	new = va_arg(args, int);
-	x = new;
-	if (new < 0)
+	va_start(v_ls, format);
+	while (format[i])
 	{
-		_write('1');
-		new = new * -1;
-		x = new;
-		b += 1;
-	}
-	while (x > 0)
-	{
-		x = x / 2;
-		b++;
-	}
-	_recursion_int_binary(new);
-	return (b);
-}
-
-/**
-  * _recursion_int_binary - Prints a binary
-  * @a: integer to print
-  *
-  */
-void _recursion_int_binary(int a)
-{
-	unsigned int t;
-
-	t = a;
-	if (t / 2)
-		_recursion_int_binary(t / 2);
-	_write(t % 2 + '0');
-}
-
-/**
-  * _print_format - Prints a format
-  * @format: The format to prints
-  * @args: A list of variadic arguments
-  *
-  * Return: The length of the format
-  */
-int _print_format(const char *format, va_list args)
-{
-	int count = 0;
-	int i = 0;
-
-	while (format && format[i])
-	{
+		buf_wr(buf);
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-				return (-1);
-
-			i++;
-
-			while (format[i] == ' ')
-				i++;
-
-			if (format[i] == '%')
-				count += _write(format[i]);
-
-			if (_validate_char(format[i]) == 0)
+			var = opid(buf, v_ls, format, i);
+			if (var < 0)
 			{
-				count = _print_invalid_spec(format[i - 1], format[i], count);
+				i = var;
+				break;
 			}
-			else
-			{
-				count += _print_spec(format[i], args);
-			}
+			i += var;
+			continue;
 		}
-		else
-		{
-			count += _write(format[i]);
-		}
-
+		buf->str[buf->index] = format[i];
+		buf_inc(buf);
 		i++;
 	}
-
-	return (count);
-}
-
-/**
-  * _print_spec - Prints a valid specifier
-  * @format: The specifier to prints
-  * @args: A list of variadic arguments
-  *
-  * Return: The length of the specifier
-  */
-int _print_spec(char format, va_list args)
-{
-	int i  = 0, length = 0;
-	spc_dt _types[] = {
-		{"c", _print_a_char},
-		{"s", _print_a_string},
-		{"d", _print_a_integer},
-		{"i", _print_a_integer},
-		{"b", _print_int_binary},
-		{NULL, NULL}
-	};
-
-	while (_types[i].specifier)
-	{
-		if (*_types[i].specifier == format)
-			length = _types[i].f(args);
-
-		i++;
-	}
-
-	return (length);
-}
-
-/**
-  * _print_invalid_spec - Prints a invalid specifier
-  * @prev_format: The previous specifier of actual specifier
-  * @format: The specifier to prints
-  * @count: The current count before prints invalid specifiers
-  *
-  * Return: The current count after prints invalid specifiers
-  */
-int _print_invalid_spec(char prev_format, char format, int count)
-{
-	count += _write('%');
-
-	if (prev_format == ' ')
-	{
-		count += _write(' ');
-		count += _write(format);
-	}
-	else
-	{
-		count += _write(format);
-	}
-
-	return (count);
-}
-
-/**
-  * _validate_char - validate the type
-  * @_type: character to be comparate
-  *
-  * Return: 1 if char is equal to a type
-  */
-int _validate_char(char _type)
-{
-	char _types[] = {'c', 's', 'd', 'i', 'b', '%'};
-	int i = 0;
-
-	while (_types[i])
-	{
-		if (_types[i] == _type)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-/**
-  * _print_a_char - Prints a char
-  * @args: A list of variadic arguments
-  *
-  * Return: The length of the character
-  */
-int _print_a_char(va_list args)
-{
-	_write(va_arg(args, int));
-	return (1);
-}
-
-/**
-  * _print_a_string - Prints a string
-  * @args: A list of variadic arguments
-  *
-  * Return: The length of the string
-  */
-int _print_a_string(va_list args)
-{
-	char *arg = va_arg(args, char *);
-	int i = 0;
-
-	if (arg != NULL)
-	{
-		while (arg[i])
-		{
-			_write(arg[i]);
-			i++;
-		}
-
-		return (i);
-	}
-
-	_write('(');
-	_write('n');
-	_write('u');
-	_write('l');
-	_write('l');
-	_write(')');
-	return (6);
-}
-
-/**
-  * _print_a_integer - Prints a integer
-  * @args: A list of variadic arguments
-  *
-  * Return: The length of the string
-  */
-int _print_a_integer(va_list args)
-{
-	int count = 1, m = 0;
-	unsigned int n = 0;
-
-	n = va_arg(args, int);
-	m = n;
-	if (m < 0)
-	{
-		_write('-');
-		m = m * -1;
-		n = m;
-		count += 1;
-	}
-	while (n > 9)
-	{
-		n = n / 10;
-		count++;
-	}
-
-	_recursion_integer(m);
-	return (count);
-}
-
-/**
-  * _recursion_integer - Prints a integer
-  * @a: integer to print
-  *
-  * Return: Nothing
-  */
-void _recursion_integer(int a)
-{
-	unsigned int t;
-
-	t = a;
-	if (t / 10)
-		_recursion_integer(t / 10);
-	_write(t % 10 + '0');
+	buf_write(buf);
+	if (var >= 0)
+		i = buf->overflow;
+	buf_end(buf);
+	va_end(v_ls);
+	return (i);
 }
